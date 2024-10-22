ad6bbbb074d824c18a187675696d547f150a4779
diego.agonzalez <diego23gonza@hotmail.com>
2024-10-22 15:27:03 -0500

    refactor

--- api-lib-common-artifact/src/main/java/com/technical/assessment/entities/AuditableEntity.java
+++ api-lib-common-artifact/src/main/java/com/technical/assessment/entities/AuditableEntity.java
@@ -3,10 +3,7 @@ package com.technical.assessment.entities;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.*;
 import lombok.experimental.SuperBuilder;
-import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedBy;
-import org.springframework.data.annotation.LastModifiedDate;
 
 import javax.persistence.*;
 import java.io.Serial;
